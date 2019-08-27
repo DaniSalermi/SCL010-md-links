d6e8b1ee652f46d3f928e7595356f1646bff55ac
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-08-26 23:28:17 -0400

    Fixing validate option and get uniques and broken links
    2 new functions, create and broken links.
    Organizing the code.
    CLI without options ok.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mdLinks = require("./md-links");
 console.time("Con validate");
-mdLinks("./README.md", { validate: true })
+mdLinks("README.md", {
+  validate: true,
+  stats: false
+})
   .then(res => {
     console.log(res);
     console.timeEnd("Con validate");
--- md-links.js
+++ md-links.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-
+// ! Dependencias necesarias del paquete
 const fs = require("fs");
 const readline = require("readline");
 const FileHound = require("filehound");
 const fetchUrl = require("fetch").fetchUrl;
-const process = require("process");
 const commander = require("commander");
-const program = new commander.Command();
-program.version("1.0.1").description("Stadistics about markdown files");
 
+// ! Fin dependencias necesarias del paquete
+
+// Variable que contiene un arreglo con todos los tipos de archivos Markdown.
 const ext = [
   "markdown",
   "mdown",
@@ -22,8 +22,109 @@ const ext = [
   "Rmd"
 ];
 
-program.parse(process.argv);
-function getFileFromDirectory(directory) {
+// ! Funciones compartidas (módulo y terminal)
+//Función que valida si es un directorio o un archivo md
+function checkMdFilesOrDirectory(path) {
+  return ext.indexOf(path.split(".").pop());
+}
+
+//Función validate, cuando se ejecuta, devuelve el status para cada link encontrado.
+function validate(link) {
+  return new Promise((resolve, reject) => {
+    let status;
+    fetchUrl(link, function(error, meta, body) {
+      status = meta.status;
+      resolve(status);
+    });
+  });
+}
+
+// Función que cuenta los links que está rotos (fail)
+function brokenLinks(links) {
+  let brokenLinksCounter = 0;
+  links.forEach(link => {
+    if (link.ok === "fail") {
+      brokenLinksCounter++;
+    }
+  });
+  return brokenLinksCounter;
+}
+//Función que retorna la cantidad de links que son únicos
+function uniqueLinks(links) {
+  let hrefs = [];
+  links.forEach(link => {
+    hrefs.push(link.href);
+  });
+  let uniqueLinks = [...new Set(hrefs)];
+  return uniqueLinks.length;
+}
+//Función que obtiene el texto dentro de 2 corchetes (texto del link encontrado)
+function getTextFromLine(line) {
+  let text = line.split("[")[1];
+  text = text.split("]")[0];
+  const maxLengthOfText = 50;
+  if (text.length > maxLengthOfText) {
+    text = text.substring(0, maxLengthOfText);
+  }
+  return text;
+}
+
+//Función que obtiene el texto dentro de 2 parentesis que contienen el texto http (URL del link encontrado)
+function getUrlFromLine(line) {
+  let url = line.split("(http")[1];
+  url = "http" + url;
+  url = url.split(")")[0];
+  return url;
+}
+
+//Esta función lee línea a líne un archivo markdown y devuelve un arreglo de objetos
+async function processLineByLine(path, options) {
+  const fileStream = fs.createReadStream(path);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  let numberOfLine = 1;
+  let numberOfLinks = 0;
+  //Se inicializa el arreglo de links vacío
+  let links = [];
+  for await (const line of rl) {
+    let linkInformation = {};
+    if (line.includes("(http") && line.includes(")")) {
+      let url = getUrlFromLine(line);
+      //Se arma el objeto con la información de cada URL
+      linkInformation = {
+        href: url,
+        text: getTextFromLine(line),
+        file: path,
+        line: numberOfLine
+      };
+      if (options.validate) {
+        linkInformation.status = await validate(url);
+        linkInformation.ok = linkInformation.status > 399 ? "fail" : "ok";
+      }
+
+      numberOfLinks++;
+      links.push(linkInformation);
+    }
+    numberOfLine++;
+  }
+  console.log(`Hay un total de: ${numberOfLinks} links`);
+  if (options.stats) {
+    let salida = "";
+    salida += `Total: ${numberOfLinks} \n`;
+    salida += `Unique: ${uniqueLinks(links)} \n`;
+    if (options.validate) {
+      salida += `Broken: ${brokenLinks(links)} \n`;
+    }
+    return salida;
+  } else {
+    return links;
+  }
+}
+
+function getFileFromDirectory(directory, options) {
+  console.log(directory);
   const files = FileHound.create()
     .paths(directory)
     .ext(ext)
@@ -43,78 +144,55 @@ function getFileFromDirectory(directory) {
     .catch(err => console.log(err));
 }
 
-function getUrlFromLine(line) {
-  let url = line.split("(h")[1];
-  url = "h" + url;
-  url = url.split(")")[0];
-  return url;
-}
+// ! Fin funciones compartidas
 
-function getTextFromLine(line) {
-  let text = line.split("[")[1];
-  text = text.split("]")[0];
-  const maxLengthOfText = 50;
-  if (text.length > maxLengthOfText) {
-    text = text.substring(0, maxLengthOfText);
-  }
-  return text;
-}
-
-function validate(link) {
+// ! Módulo del require
+module.exports = (path, options = { validate: false, stats: false }) => {
   return new Promise((resolve, reject) => {
-    let status;
-    fetchUrl(link, function(error, meta, body) {
-      status = meta.status;
-      resolve(status);
-    });
-  });
-}
-
-async function processLineByLine(path, options) {
-  const fileStream = fs.createReadStream(path);
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  let numberOfLine = 1;
-  let numberOfLinks = 0;
-  let links = [];
-  for await (const line of rl) {
-    let linkInformation = {};
-    if (line.includes(")") && line.includes("(http")) {
-      let url = getUrlFromLine(line);
-      linkInformation = {
-        href: url,
-        text: getTextFromLine(line),
-        file: path,
-        line: numberOfLine
-      };
-      if (options.validate) {
-        linkInformation.status = await validate(url);
-      }
-
-      numberOfLinks++;
-      links.push(linkInformation);
-    }
-    numberOfLine++;
-  }
-  console.log(`Hay un total de: ${numberOfLinks} links`);
-  return links;
-}
-
-module.exports = (path, options = { validate: false }) => {
-  return new Promise((resolve, reject) => {
-    const isMd = ext.indexOf(path.split(".").pop());
+    console.log(path);
+    const isMd = checkMdFilesOrDirectory(path);
     if (isMd >= 0) {
       resolve(processLineByLine(path, options));
     } else {
-      reject("No hay archivos de tipo markdown");
+      resolve(getFileFromDirectory(path, options));
+      //reject("No hay archivos de tipo markdown");
     }
   });
 };
 
-const cliEjecution = () => {
-  getFileFromDirectory(program.args[0]);
-};
+// ! Fin módulo require
 
-cliEjecution();
+// ! Terminal (CLI)
+
+const program = new commander.Command();
+program.version("1.0.1").description("Stadistics about markdown files");
+
+//Definiendo opciones válidas que podrá ingresar el usuario
+program
+  .option("-v, --validate", "to validate the links inside of a markdown file")
+  .option(
+    "-s, --stats",
+    "to show some basic stats about the links (total of links and unique ones"
+  );
+
+program.parse(process.argv);
+
+console.log(program.args);
+
+const cliEjecution = () => {
+  const isMd = checkMdFilesOrDirectory(program.args[0]);
+  if (isMd >= 0) {
+    processLineByLine(program.args[0]);
+  } else {
+    getFileFromDirectory(program.args[0]);
+  }
+
+  if (program.validate) {
+  }
+};
+// Se determina si se está haciendo una ejecución por CLI
+if (program.args.length > 0) {
+  cliEjecution();
+}
+
+// ! Fin funciones terminal (CLI)
